df9a9457078408c70bd92d311067fdb3215d9770
lovinaprimiere <gta.lovinapremiere@gmail.com>
2024-07-21 18:22:08 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,10 +87,49 @@ const contactForm = document.getElementById('contact-form'),
  contactForm.addEventListener('submit', sendEmail)
  
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal ({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true // Reset Animation
+})
+
+sr.reveal(`.home__data, .community, .contact__container`)
+sr.reveal(`.home__img`, {delay: 600})
+sr.reveal(`.home__scroll`, {delay: 1000})
+sr.reveal(`.work__card, .service__card`, {interval: 100})
+sr.reveal(`.website__content`, {origin: 'right'})
+sr.reveal(`.about__img`, {origin: 'left'})
